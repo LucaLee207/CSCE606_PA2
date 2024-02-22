8ede80d5f37004ae50211380aa422719432132b7
Luca Lee <kartg0019246@gmail.com>
2024-02-21 22:08:59 -0600

    update seed value

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,13 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Inception', :rating => 'PG-13',
+    :release_date => '13-Jul-2010'},
+  {:title => 'Tenet', :rating => 'PG-13',
+    :release_date => '3-Sep-2020'},
+  {:title => 'Oppenheimer', :rating => 'R',
+    :release_date => '21-Jul-2021'}
 ]
 
 more_movies.each do |movie|
